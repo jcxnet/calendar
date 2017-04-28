9fa4ee0360de20f193560956ecd2487605a1bb1c
Jcxnet <jcxnet@gmail.com>
2017-04-28 15:17:38 -0500

    Initial commit: - guzzlehttp added - boostrap added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+@section('content')
+    <h1>Content</h1>
+    <p>{{$data}}</p>
+@endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calendar - exercise</title>
+    <link href="{{app('url')->asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="{{app('url')->asset('js/html5shiv.min.js')}}"></script>
+    <script src="{{app('url')->asset('js/respond.min.js')}}"></script>
+    <![endif]-->
+</head>
+<body class="container">
+    <div class="row">
+        <div class="col-md-12">
+            @section('content')
+            @show
+        </div>
+    </div>
+<script src="{{ app('url')->asset('js/jquery-3.2.1.min.js')  }}"></script>
+<script src="{{ app('url')->asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
